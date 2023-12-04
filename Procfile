13f78bb251ab9c74e73431056774c4c4319840e7
Caroline Danielsson <caroline.e.danielsson@gmail.com>
2023-12-04 07:27:59 +0000

    Deployment commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn bjorkhem.wsgi:application
--- bjorkhem/settings.py
+++ bjorkhem/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-cardan22-bjorkhem-5e8ey0hewkk.ws-eu106.gitpod.io']
+ALLOWED_HOSTS = ['8000-cardan22-bjorkhem-5e8ey0hewkk.ws-eu106.gitpod.io', 'bjorkhem-8b5ef3ef81a6.herokuapp.com']
 
 
 # Application definition
