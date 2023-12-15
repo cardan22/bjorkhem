9e78dadd55da475d64f8d3089fb99105469b5305
Caroline Danielsson <caroline.e.danielsson@gmail.com>
2023-12-15 10:49:52 +0000

    Add favorite functionality to icons and Implement favorite functionality and authentication for user interaction

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<product_id>', views.product_detail, name='product_detail'),
-    path('favorites/', views.favorite_products, name='favorite_products'),
+    path('<product_id>', views.product_detail.as_view(), name='product_detail'),
+    path('favorites/', views.favorite_products.as_view(), name='favorite_products'),
+    path(
+        'add-favorite-product/<int:product_id>/', views.add_favorite_product.as_view(),
+        name="add_favorite_product"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import View
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 from .models import Product
 
 
@@ -14,25 +18,59 @@ def all_products(request):
     return render(request, 'products/products.html', context)
 
 
-def product_detail(request, product_id):
+class product_detail(View):
     """ A view to show individual product details """
 
-    product = get_object_or_404(Product, pk=product_id)
+    template_name = 'products/product_detail.html'
 
-    context = {
-        'product': product,
-    }
+    def get(self, request, product_id):
+        product = get_object_or_404(Product, id=product_id)
 
-    return render(request, 'products/product_detail.html', context)
+        favorite = product.favorites.filter(id=request.user.id).exists()
+
+        context = {
+            'product': product,
+            'favorite': favorite,
+        }
+
+        return render(request, self.template_name, context)
 
 
-def favorite_products(request):
+class add_favorite_product(LoginRequiredMixin, View):
+    """
+    View for adding/removing products from user's favorites.
+    Allows authenticated users to add or remove products from their favorites.
+    If already a favorite, it's removed; otherwise, it's added.
+    """
+
+    def get(self, request, product_id):
+        product = get_object_or_404(Product, id=product_id)
+
+        is_favorite = product.favorites.filter(id=request.user.id).exists()
+
+        if is_favorite:
+            product.favorites.remove(request.user)
+            msg = "This product was removed from your favorites."
+            messages.warning(request, msg)
+        else:
+            product.favorites.add(request.user)
+            msg = "This product was added to your favorites."
+            messages.success(request, msg)
+
+        return redirect(request.META.get('HTTP_REFERER'))
+
+
+class favorite_products(LoginRequiredMixin, ListView):
     """ A view to show favorite products """
+    model = Product
+    template_name = 'products/favorite_products.html'
+    context_object_name = 'favorite_products'
 
-    favorite_products = request.user.favorite_products.all()
-
-    context = {
-        'products': favorite_products,
-    }
-
-    return render(request, 'products/favorite_products.html', context)   
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            user = self.request.user
+            favorite_products = user.favorite_products.all()
+            context['favorite_products'] = favorite_products
+        return context
+   
