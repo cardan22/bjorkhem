acd1850dad007564ad9fb394121b90e0d71a8be2
Caroline Danielsson <caroline.e.danielsson@gmail.com>
2023-12-21 08:48:29 +0000

    Add bag context and delivery logic

--- bjorkhem/settings.py
+++ bjorkhem/settings.py
@@ -85,6 +85,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
@@ -196,6 +197,9 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
+FREE_DELIVERY_THRESHOLD = 499
+STANDARD_DELIVERY_PERCENTAGE = 10    
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
