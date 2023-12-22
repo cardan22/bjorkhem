f0c00b0953cf4caa21e4a8aa96f228ce2b28ab75
Caroline Danielsson <caroline.e.danielsson@gmail.com>
2023-12-22 14:13:53 +0000

    Add discount calculation to context and update bag template with +/- quantity

--- bag/contexts.py
+++ bag/contexts.py
@@ -12,12 +12,15 @@ def bag_contents(request):
 
     bag_items = []
     total = 0
+    total_discount = 0
     product_count = 0
     bag = request.session.get('bag', {})
 
     for item_id, quantity in bag.items():
         product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
+        regular_price = product.price * quantity
+        total_discount = (product.price - product.discount) * quantity
+        total = regular_price
         product_count += quantity
         bag_items.append({
             'item_id': item_id,
@@ -32,7 +35,7 @@ def bag_contents(request):
         delivery = 0
         free_delivery_delta = 0
     
-    grand_total = delivery + total
+    grand_total = total + total_discount + delivery
     
     context = {
         'bag_items': bag_items,
@@ -42,6 +45,7 @@ def bag_contents(request):
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
+        'total_discount': total_discount,
     }
 
     return context
