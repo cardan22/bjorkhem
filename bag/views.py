1e0daae2c1cfb3c574a6cbf55f9f72164dc5118b
Caroline Danielsson <caroline.e.danielsson@gmail.com>
2023-12-28 10:17:30 +0000

    Add adjust bag and remove from bag views, urls and js to bag template

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_bag(request):
@@ -21,4 +21,40 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
 
-    return redirect(redirect_url)    
+    return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    # Check if the item is already in the bag
+    if item_id in bag:
+        # Update quantity
+        if quantity > 0:
+            bag[item_id] = quantity
+        # Remove item if quantity is 0 or less
+        else:
+            bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        bag = request.session.get('bag', {})
+
+        # Remove item from the bag
+        bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
+
