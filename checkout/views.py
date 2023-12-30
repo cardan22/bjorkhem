be25795710319b05992deb9e630f8ab499488f6a
Caroline Danielsson <caroline.e.danielsson@gmail.com>
2023-12-30 10:19:00 +0000

    Add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51ODRGID60gEJ1KcVUZz1wY2ieofoVFxnoAJaWUAU9Q3MtyDo7eb47haHzQOjc46eCi3NdatxBbwEjCWNi32txhEO00VB2c9VPs',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
