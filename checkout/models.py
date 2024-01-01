3c90908a02a44e64db62636a4d12ecd2e8fc2ad8
Caroline Danielsson <caroline.e.danielsson@gmail.com>
2024-01-01 10:43:16 +0000

    Add checkout form data caching in payment intent

--- checkout/models.py
+++ checkout/models.py
@@ -8,7 +8,7 @@ from products.models import Product
 
 
 class Order(models.Model):
-    order_number = models.CharField(max_length=10, null=False, editable=False)
+    order_number = models.CharField(max_length=20, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
