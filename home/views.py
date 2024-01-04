f7a550fa4356bb647770c5dfd1d987c765a7195a
Caroline Danielsson <caroline.e.danielsson@gmail.com>
2024-01-04 15:53:27 +0000

    Add latest news section and views to home template

--- home/views.py
+++ home/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from products.models import Product
+
 
 def index(request):
-    """ A view to return the index page """
+    """ A view to return the index page with latest news """
 
-    return render(request, 'home/index.html')
+    news_products = Product.objects.filter(category__name='News')[:4]
+
+    context = {
+        'news_products': news_products,
+    }
+
+    return render(request, 'home/index.html', context)
+
