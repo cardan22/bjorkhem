ff47c9930362b1332d0d06917665eb9ac965f644
Caroline Danielsson <caroline.e.danielsson@gmail.com>
2024-01-07 15:41:20 +0000

    Add purchased_quantity to checkout models

--- checkout/models.py
+++ checkout/models.py
@@ -77,6 +77,7 @@ class OrderLineItem(models.Model):
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+    purchased_quantity = models.IntegerField(null=True, blank=True, default=0)
 
     def save(self, *args, **kwargs):
         """
