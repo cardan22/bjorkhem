21e6bd036d68634a099c400b93929f691fd7c7c6
Caroline Danielsson <caroline.e.danielsson@gmail.com>
2024-01-09 08:13:41 +0000

    Turn off DEBUG mode in settings

--- bjorkhem/settings.py
+++ bjorkhem/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-cardan22-bjorkhem-wcr7bo57lxe.ws-eu107.gitpod.io',
