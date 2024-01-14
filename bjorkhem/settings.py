ccd74ce49b3e13bf22dae7b2b47e102a2379cfa6
Caroline Danielsson <caroline.e.danielsson@gmail.com>
2024-01-14 09:32:14 +0000

    Fix signup error in allauth by reformatting AUTH_PASSWORD_VALIDATORS

--- bjorkhem/settings.py
+++ bjorkhem/settings.py
@@ -139,25 +139,18 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': (
-            'django.contrib.auth.password_validation.'
-            'UserAttributeSimilarityValidator',
-        ),
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
@@ -234,5 +227,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-cardan22-bjorkhem-iucyymraw32.ws-eu107.gitpod.io/*',
-    'https://bjorkhem-8b5ef3ef81a6.herokuapp.com/*'
+    'https://bjorkhem-8b5ef3ef81a6.herokuapp.com/*',
 ]
